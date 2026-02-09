1667b34ce1ce54831df4e5927457dbd330eb9f6d
Kushagra614 <kushagravardhan619@gmail.com>
2026-02-09 14:14:35 +0530

    Add flex layout to Index for better rendering

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,11 +3,11 @@ import Hero from '@/components/Hero';
 
 const Index = () => {
   return (
-    <div className="min-h-screen bg-background text-foreground">
+    <div className="min-h-screen bg-background text-foreground flex flex-col">
       <Navigation />
-      <main className="relative">
+      <main className="relative flex-1">
         {/* Hero Section */}
-        <section className="relative z-10">
+        <section className="relative z-10 w-full">
           <Hero />
         </section>
       </main>
